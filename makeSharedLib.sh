8805bfb8f2864211899c379641065bdab10d877a
kyamada <kentarou06@gmail.com>
2012-09-01 19:57:33 +0900

    create shellscript for shared library

---
+++ makeSharedLib.sh
@@ -0,0 +1,14 @@
+
+list_file="include_source.txt"
+output="myLib.so"
+
+files=`cat ${list_file}`
+arg=""
+for file in $files
+do
+    arg="${arg} ${file}"
+#    arg="${arg}-c ${file} "
+done
+
+echo "g++ -shared${arg} -o ${output} -fPIC"
+g++ -shared ${arg} -o ${output} -fPIC
