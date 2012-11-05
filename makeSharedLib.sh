9d1e0171f34b7f78578d005f888f11b7058201b4
kentaro YAMADA <kentarou06@gmail.com>
2012-11-05 10:35:06 +0900

    change compile way to remove unneeded files

--- makeSharedLib.sh
+++ makeSharedLib.sh
@@ -11,20 +11,43 @@ arg=""
 objList=""
 i=0;
 MYLIB="${HOME}/lib/myCppLib"
+
+if [ -f ${output} ]
+then
+    mv ${output} ${output}.bak
+fi
+
+compileFlag=1
 for file in $files
 do
     objFile="obj${i}.o"
+    if [ -f ${objFile} ]
+    then
+	rm ${objFile}
+    fi
     echo "g++ -fPIC -c $file -o ${objFile} -I${MYLIB}"
     g++ -fPIC -c $file -o ${objFile} -I${MYLIB}
 
-    objList="${objList} ${objFile}"
+    if [ -f ${objFile} ]
+    then
+	objList="${objList} ${objFile}"
+    else
+	compileFlag=0
+    fi
     i=`expr $i + 1`
 done
 
-echo "g++ -shared ${objList} -o ${output}"
-g++ -shared ${objList} -o ${output}
+if [ ${compileFlag} -eq 1 ]
+then
+    echo "g++ -shared ${objList} -o ${output}"
+    g++ -shared ${objList} -o ${output}
+    echo "making headerfile.h ... "
+    sh findHeader.sh
+else
+    echo ""
+    echo "compile failed"
+fi
 
+rm ${objList}
 cd ${nowDir}
 
-echo "making headerfile.h ... "
-sh findHeader.sh
